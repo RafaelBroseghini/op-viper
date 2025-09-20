eacd69fec2e6891567b594918a735da79bb3e0c9
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2025-09-20 17:33:48 -0500

    allows passing opt funcs; adds ci workflow

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/rafaelbroseghini/op-viper
 
-go 1.23.4
+go 1.25.1
 
 require (
 	github.com/1password/onepassword-sdk-go v0.3.1
--- pkg/onepassword/onepassword.go
+++ pkg/onepassword/onepassword.go
@@ -10,13 +10,62 @@ import (
 	"github.com/go-viper/mapstructure/v2"
 )
 
+const (
+	OpenCurlyBrace  = "{{"
+	CloseCurlyBrace = "}}"
+)
+
+type Loader struct {
+	Prefix string
+	Suffix string
+	Client OnePasswordClient
+}
+
+type LoaderFunc func(h *Loader)
+
+func NewDefaultLoader() Loader {
+	return Loader{Prefix: OpenCurlyBrace, Suffix: CloseCurlyBrace, Client: OnePasswordCLIClient{}}
+}
+
+func NewLoader(opts ...LoaderFunc) Loader {
+	h := Loader{}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
+}
+
+func WithPrefix(prefix string) LoaderFunc {
+	return func(h *Loader) {
+		h.Prefix = prefix
+	}
+}
+
+func WithSuffix(suffix string) LoaderFunc {
+	return func(h *Loader) {
+		h.Suffix = suffix
+	}
+}
+
+func WithCLIClient(client OnePasswordCLIClient) LoaderFunc {
+	return func(h *Loader) {
+		h.Client = client
+	}
+}
+
+func WithSDKClient(client OnePasswordSDKClient) LoaderFunc {
+	return func(h *Loader) {
+		h.Client = client
+	}
+}
+
 type OnePasswordClient interface {
-	Get(ctx context.Context, path string) (string, error)
+	Resolve(ctx context.Context, path string) (string, error)
 }
 
 type OnePasswordCLIClient struct{}
 
-func (c OnePasswordCLIClient) Get(ctx context.Context, path string) (string, error) {
+func (c OnePasswordCLIClient) Resolve(ctx context.Context, path string) (string, error) {
 	cmd := exec.CommandContext(ctx, "op", "read", path)
 	output, err := cmd.Output()
 	if err != nil {
@@ -37,7 +86,7 @@ func NewOnePasswordSDKClient(ctx context.Context, name, version, token string) O
 	return OnePasswordSDKClient{Client: client}
 }
 
-func (c OnePasswordSDKClient) Get(ctx context.Context, path string) (string, error) {
+func (c OnePasswordSDKClient) Resolve(ctx context.Context, path string) (string, error) {
 	item, err := c.Client.Secrets().Resolve(ctx, path)
 	if err != nil {
 		return "", err
@@ -45,7 +94,7 @@ func (c OnePasswordSDKClient) Get(ctx context.Context, path string) (string, err
 	return item, nil
 }
 
-func OnePasswordHookFunc(ctx context.Context, c OnePasswordClient) mapstructure.DecodeHookFunc {
+func (h Loader) OnePasswordHookFunc(ctx context.Context) mapstructure.DecodeHookFunc {
 	return func(f reflect.Type, t reflect.Type, data any) (any, error) {
 		if f.Kind() != reflect.String {
 			return data, nil
@@ -56,18 +105,18 @@ func OnePasswordHookFunc(ctx context.Context, c OnePasswordClient) mapstructure.
 			return data, nil
 		}
 
-		if !strings.HasPrefix(value, "{{") || !strings.HasSuffix(value, "}}") {
+		if !strings.HasPrefix(value, h.Prefix) || !strings.HasSuffix(value, h.Suffix) {
 			return value, nil
 		}
 
-		value = strings.TrimPrefix(value, "{{")
-		value = strings.TrimSuffix(value, "}}")
+		value = strings.TrimPrefix(value, h.Prefix)
+		value = strings.TrimSuffix(value, h.Suffix)
 		value = strings.TrimSpace(value)
 
 		if !strings.HasPrefix(value, "op://") {
 			return value, nil
 		}
 
-		return c.Get(ctx, value)
+		return h.Client.Resolve(ctx, value)
 	}
 }
